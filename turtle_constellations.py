2a3ff9ffa2a7e384645d76c57068e53e8109c97b
ezetina86 <jenzetin@gmail.com>
2019-02-20 17:58:21 -0600

    Excercises with turtles!

---
+++ turtle_constellations.py
@@ -0,0 +1,18 @@
+import turtle
+
+#Position yto start my draw
+
+window = turtle.Screen()
+
+tortuga = turtle.Turtle()
+
+tortuga.hideturtle()
+
+tortuga.goto(-200,-100)
+
+tortuga.showturtle()
+
+tortuga.forward(100)
+
+turtle.mainloop()
+
---
+++ turtle_squere.py
@@ -0,0 +1,23 @@
+import turtle
+
+window  = turtle.Screen()
+
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+
+turtle.mainloop()
+
+
+
