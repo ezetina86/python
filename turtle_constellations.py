0fe66cc8070cca27dc961102189c572e67432a11
ezetina86 <jenzetin@gmail.com>
2019-02-22 09:44:04 -0600

    draw of teh  Great Bear Cosntelattion seem from earth

--- turtle_constellations.py
+++ turtle_constellations.py
@@ -1,18 +1,69 @@
 import turtle
 
-#Position yto start my draw
 
-window = turtle.Screen()
+print 'Welcome lets draw the Great Bear Constellation'
+print 'Using a turtle'
+print 'Please choose an option:'
 
-tortuga = turtle.Turtle()
+print '1. Great Bear Cosntelattion seem from earth'
+print '0. Exit'
 
-tortuga.hideturtle()
+opt = input()
 
-tortuga.goto(-200,-100)
+if opt == 1:
 
-tortuga.showturtle()
+    # Configuring the turtle
 
-tortuga.forward(100)
+    window = turtle.Screen()
 
-turtle.mainloop()
+    turtle.setup(800, 800)
 
+    t = turtle.Turtle()
+    # Put label on the  top
+    #t.tiltle("Great Bear constellation")
+    # Selecting Turtle shape
+    t.shape("turtle")
+    # Selecting color of my turtle
+    t.color("green")
+    t.pen()
+
+    ############################################################################################
+    # This is a  draw of teh  Great Bear Cosntelattion seem from earth
+
+    t.dot()
+
+    t.left(5)
+    t.forward(100)
+    t.dot()
+
+    t.right(45)
+    t.forward(100)
+    t.dot()
+
+    t.right(7)
+    t.forward(50)
+    t.dot()
+
+    t.right(30)
+    t.forward(50)
+    t.dot()
+
+    t.left(82)
+    t.forward(100)
+    t.dot()
+
+    t.left(70)
+    t.forward(50)
+    t.dot()
+
+    # Thats it
+    ############################################################################################
+
+    turtle.exitonclick()
+
+
+elif opt == 0:
+    print('Bye bitch!')
+
+else:
+    print('Error check it out!')
