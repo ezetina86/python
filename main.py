9b29ff6e2ae9ad45b2e6abb0c6a6fa62a8ed5e01
ezetina86 <jenzetin@gmail.com>
2019-03-20 16:44:43 -0600

    Adding update function

--- main.py
+++ main.py
@@ -23,13 +23,20 @@ def _add_comma():
     clients += ','
 
 # Function to list clients
-
-
 def list_clients():
     global clients
 
     print(clients)
 
+
+# Update clients recives a client name to update it 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',') 
+    else:
+        print('Client not found!')
 
 # Function to print a welcome message
 def _print_welcome():
@@ -43,7 +50,12 @@ def _print_welcome():
 
     print('[C]reate client')
     print('[D]elete client')
+    print('[U]pdate client')
 
+
+# Private method  to get the  client name
+def _get_client_name():
+    return input('Please type the client name... ')
 
 # main function calls crate_client
 if __name__ == '__main__':
@@ -51,13 +63,19 @@ if __name__ == '__main__':
 
  # Stop the execution until we have a option selected
     command = input()
+    command = command.upper()
 
  # Lets check what the user want to do
     if command == 'C':
-        client_name = input('Please type the client name... ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+         client_name = _get_client_name()
+         updated_client_name = input('Please type the new client name...')
+         update_client(client_name,updated_client_name) 
     else:
         print('Invalid option')
+        
